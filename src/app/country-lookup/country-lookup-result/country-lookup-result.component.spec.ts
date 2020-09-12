9fec441fb18d359264529f98cfce20db5f46677b
Rob DC <moppet2k@gmail.com>
2020-09-12 01:12:24 +0100

    added some tests

--- src/app/country-lookup/country-lookup-result/country-lookup-result.component.spec.ts
+++ src/app/country-lookup/country-lookup-result/country-lookup-result.component.spec.ts
@@ -1,6 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CountryLookupResultComponent } from './country-lookup-result.component';
+import { CountryDetails } from '../shared/country-details.model';
+
+const mockCountry: CountryDetails = {
+  id: 'GBR',
+  iso2Code: 'GB',
+  name: 'United Kingdom',
+  capitalCity: 'London',
+  longitude: '-0.126236',
+  latitude: '51.5002',
+  region: {
+    id: '',
+    iso2code: '',
+    value: ''
+  },
+  adminregion: {
+    id: '',
+    iso2code: '',
+    value: ''
+  },
+  incomeLevel: {
+    id: '',
+    iso2code: '',
+    value: ''
+  },
+  lendingType: {
+    id: '',
+    iso2code: '',
+    value: ''
+  }
+};
+
 
 describe('CountryLookupResultComponent', () => {
   let component: CountryLookupResultComponent;
@@ -8,9 +39,9 @@ describe('CountryLookupResultComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CountryLookupResultComponent ]
+      declarations: [CountryLookupResultComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +52,39 @@ describe('CountryLookupResultComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('@Input details', () => {
+    [null, undefined].forEach(value => {
+      it('should not show table if the details are falsy', () => {
+        component.countryCode = value;
+        fixture.detectChanges();
+        const table = fixture.nativeElement.querySelector('table');
+
+        expect(table).toBeNull();
+      });
+    });
+
+    it('should show table if the details are provided', () => {
+      component.details = mockCountry;
+      fixture.detectChanges();
+      const table = fixture.nativeElement.querySelector('table');
+      const name = fixture.nativeElement.querySelector('#result-table-country-name');
+
+      expect(table).not.toBeNull();
+      expect(name.textContent).toEqual(mockCountry.name);
+    });
+  });
+
+  describe('@Input countryCode', () => {
+    it('should show correct country code', () => {
+      component.countryCode = 'XX';
+      component.details = mockCountry;
+      fixture.detectChanges();
+      const table = fixture.nativeElement.querySelector('table');
+      const resultText = fixture.nativeElement.querySelector('#result-user-value-used');
+
+      expect(resultText.textContent).toEqual('Result for "XX"');
+    });
   });
 });
--- src/app/country-lookup/shared/country-lookup.service.spec.ts
+++ src/app/country-lookup/shared/country-lookup.service.spec.ts
@@ -1,13 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CountryLookupService } from './country-lookup.service';
+import { CountryLookupService, CountryFailureReason } from './country-lookup.service';
 import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs/internal/observable/of';
+import { CountryLookupApiResponse } from './country-lookup-api-response.model';
+import { throwError } from 'rxjs';
 
 describe('CountryLookupService', () => {
   let service: CountryLookupService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    const httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'delete', 'put']);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'delete', 'put']);
 
     TestBed.configureTestingModule({
       providers: [{ provide: HttpClient, useValue: httpClientSpy }]
@@ -17,5 +21,52 @@ describe('CountryLookupService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('::getCountry()', () => {
+    it('should add supplied country code to URL', () => {
+      httpClientSpy.get.and.returnValue(of([[], []]));
+      service.getCountry('XX');
+
+      expect(httpClientSpy.get.calls.mostRecent().args[0]).toMatch(/v2\/country\/XX/);
+    });
+
+    it('should include format=json parameter in API call', () => {
+      httpClientSpy.get.and.returnValue(of([[], []]));
+      service.getCountry('XX');
+
+      expect(httpClientSpy.get.calls.mostRecent().args[1]).toEqual({ params: { format: 'json' } });
+    });
+
+    it('should return failure NOT_FOUND when api does not return a country and message id is "120"', (done: DoneFn) => {
+      const apiResponse: CountryLookupApiResponse = [{ message: [{ id: '120', key: '', value: '' }] }];
+      httpClientSpy.get.and.returnValue(of(apiResponse));
+
+      service.getCountry('XX').subscribe(result => {
+        expect(result).toEqual(CountryFailureReason.NOT_FOUND);
+        done();
+      });
+    });
+
+    it('should return failure UNKOWN when api does not return a country and message id is NOT "120"', (done: DoneFn) => {
+      const apiResponse: CountryLookupApiResponse = [{ message: [{ id: '999', key: '', value: '' }] }];
+      httpClientSpy.get.and.returnValue(of(apiResponse));
+
+      service.getCountry('XX').subscribe(result => {
+        expect(result).toEqual(CountryFailureReason.UNKNOWN);
+        done();
+      });
+    });
+
+    it('should not swallow error if API returns error code', (done: DoneFn) => {
+      httpClientSpy.get.and.returnValue(throwError('boom'));
+
+      service.getCountry('XX').subscribe(
+        () => { },
+        error => {
+          expect(error).toEqual('boom');
+          done();
+        });
+    });
   });
 });
